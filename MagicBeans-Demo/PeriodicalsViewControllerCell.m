056c2ba2c6cdc8b9acd6388e3181b12b4aa1b44b
Magic Beans <MagicBeans@MagicdeMac-mini-2.local>
2015-01-15 16:42:29 +0800

    1.调整期刊名称

--- MagicBeans-Demo/PeriodicalsViewControllerCell.m
+++ MagicBeans-Demo/PeriodicalsViewControllerCell.m
@@ -23,12 +23,20 @@
 
 - (void)initWithArray:(NSArray *)itemsArray withIndex:(NSInteger)itemIndex
 {
+    NSDateFormatter *formater = [[NSDateFormatter alloc] init];
+
     if (itemsArray.count > 0) {
         NSDictionary *leftDic = itemsArray[0];
         [self.leftItemView setHidden:NO];
         [self.leftItemBtn setImageForState:UIControlStateNormal withURL:[NSURL URLWithString:leftDic[@"path"]] placeholderImage:nil];
         self.leftItemBtn.tag = itemIndex;
-        self.leftDateLabel.text = leftDic[@"date"];
+        [formater setDateFormat:@"yyyy-M-dd"];
+        NSDate *date = [formater dateFromString:leftDic[@"date"]];
+        [formater setDateFormat:@"yyyy"];
+        NSString *year = [formater stringFromDate:date];
+        [formater setDateFormat:@"M"];
+        NSString *month = [formater stringFromDate:date];
+        self.leftDateLabel.text = [NSString stringWithFormat:@"%@年第%@期",year,month];
     }
     else
     {
@@ -40,7 +48,13 @@
         [self.centerItemView setHidden:NO];
         [self.centerItemBtn setImageForState:UIControlStateNormal withURL:[NSURL URLWithString:centerDic[@"path"]] placeholderImage:nil];
         self.centerItemBtn.tag = itemIndex + 1;
-        self.centerDateLabel.text = centerDic[@"date"];
+        [formater setDateFormat:@"yyyy-M-dd"];
+        NSDate *date = [formater dateFromString:centerDic[@"date"]];
+        [formater setDateFormat:@"yyyy"];
+        NSString *year = [formater stringFromDate:date];
+        [formater setDateFormat:@"M"];
+        NSString *month = [formater stringFromDate:date];
+        self.centerDateLabel.text = [NSString stringWithFormat:@"%@年第%@期",year,month];
     }
     else
     {
@@ -52,7 +66,13 @@
         [self.rightItemView setHidden:NO];
         [self.rightItemBtn setImageForState:UIControlStateNormal withURL:[NSURL URLWithString:rightDic[@"path"]] placeholderImage:nil];
         self.rightItemBtn.tag = itemIndex + 2;
-        self.rightDateLabel.text = rightDic[@"date"];
+        [formater setDateFormat:@"yyyy-mm-dd"];
+        NSDate *date = [formater dateFromString:rightDic[@"date"]];
+        [formater setDateFormat:@"yyyy"];
+        NSString *year = [formater stringFromDate:date];
+        [formater setDateFormat:@"mm"];
+        NSString *month = [formater stringFromDate:date];
+        self.rightDateLabel.text = [NSString stringWithFormat:@"%@年第%@期",year,month];
     }
     else
     {
