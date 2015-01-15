116063046bcdfaa07c07be39f90a26581a33da76
sylar-MagicBeans <sylar@MagicdeMac-mini.local>
2015-01-15 16:18:55 +0800

    1.add wangshang jubao

--- MagicBeans-Demo/ReportViewController.m
+++ MagicBeans-Demo/ReportViewController.m
@@ -23,6 +23,9 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)WangshangButtonPressed:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.scnjyfw.com"]];
+}
 
 /*
 #pragma mark - Navigation
